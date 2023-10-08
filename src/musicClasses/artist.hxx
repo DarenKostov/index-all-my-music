56bb82a1896752a7c1039ad55f591458b17a3ea3
DarenKostov <daren.kostov@outlook.com>
2023-10-07 20:43:40 -0700

    artist class header

---
+++ src/musicClasses/artist.hxx
@@ -0,0 +1,41 @@
+/*
+Copyright (c) 2023 Daren Kostov
+
+This file is part of index-all-my-musi
+
+index-all-my-musi is free software: you can redistribute it and/or modify it under the terms
+of the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+index-all-my-musi is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with index-all-my-musi
+If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
+
+#ifndef ARTIST
+#define ARTIST
+
+#include "music/baseMusic.hxx"
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+
+
+class Artist{
+
+  private:
+    std::string name;
+    std::vector<BaseMusic*> musicMade;
+    std::unordered_map<std::string, std::string> contactsOrWebsites;
+
+  public:
+  
+};
+
+#endif
